1eccad1d53801de185658d0eac75034f3b3d8fad
hind <agtay.hind@gmail.com>
2020-03-30 20:24:01 +0100

    add front end

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
 # Create your views here.
 def home(request):
 	return render(request,'base.html')
+def new_search(request):
+	search = request.POST.get('search')
+	
+	front_end = {
+		'search':search,
+	}
+
+	return render(request,'my_app/new_search.html',front_end)
